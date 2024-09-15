9af406f1f1dfc83e43818c7b0927df4145502881
dacmarcell <devmarcell@protonmail.com>
2024-09-15 04:03:38 -0300

    feat: create calculo diaria strategy

---
+++ src/test/java/com/dacti/CalculoDiaria.java
@@ -0,0 +1,14 @@
+package com.dacti;
+
+public class CalculoDiaria implements Calculo {
+    private int valorDiaria;
+
+    public CalculoDiaria(int valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+
+    @Override
+    public int calcularTarifa(int qtdHoras) {
+        return valorDiaria * (int) Math.ceil(qtdHoras / 24.0);
+    }
+}
